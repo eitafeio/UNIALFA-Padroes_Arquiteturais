918df3df3cf9f3b9c9fb7dcf9aa4ce50a9033d1b
Pancieri <thiagopancieri@gmail.com>
2024-04-15 19:17:51 -0300

    Criação da classe Taxa

--- comprovante_pagamento.py
+++ comprovante_pagamento.py
@@ -2,7 +2,38 @@
 # 5 atributos e 3 métodos para cada classe
 
 class Taxa:
-    pass      
+    """
+    Classe que representa uma taxa de acordo com um modelo de comprovante de pagamento.
+    """
+def __init__(self, nome, valor, tipo, data_inicio, data_fim):
+    self.nome = nome
+    self.valor = valor
+    self.tipo = tipo
+    self.data_inicio = data_inicio
+    self.data_fim = data_fim
+    
+def aplicar_taxa(self, valor_original):
+    """
+    Aplica a taxa ao valor original e retorna o valor final
+        
+    :param valor_original: O valor original antes da aplicação da taxa
+    :return: O valor final após a aplicação da taxa
+    """
+        
+def esta_ativa(self, data):
+    """
+    Verifica se a taxa está ativa em uma determinada data
+
+    :param data: A data a ser verificada
+    :return: True se a taxa estiver ativa, False caso contrário
+    """
+        
+def imprimir_detalhes(self): 
+    """
+    Imprime os detalhes da taxa
+    """
+
+    
 class MetodoDePagamento:
     pass
 class StatusDePagamento:
