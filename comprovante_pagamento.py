4c0af0e9dcc473a271c2dc79b2f6e7f920d2388d
Thiago Pancieri <thiagopancieri@gmail.com>
2024-04-15 15:11:45 -0300

    Criação de quatro classes para o modelo de comprovante de pagamento

---
+++ comprovante_pagamento.py
@@ -0,0 +1,11 @@
+# 30 classes em cima do modelo de comprovante de pagamento feito em sala
+# 5 atributos e 3 métodos para cada classe
+
+class Taxa:
+    pass      
+class MetodoDePagamento:
+    pass
+class StatusDePagamento:
+    pass
+class ReciboDePagamento:
+    pass
